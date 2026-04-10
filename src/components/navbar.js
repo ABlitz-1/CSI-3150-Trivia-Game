4fe6ffa9dea5b8492057ff1d02a9118b926e7e93
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-10 12:41:13 -0400

    create navbar.js and update question.js

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-none">
+      <div className="container">
+        <a clasName="navbar-brand text-light fw-bolder fs-3" href="/">
+          Guess It Trivia
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
