6e850495dc47557b91fd681513ea3b29f624d1f7
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-10 18:59:11 -0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    commonjsOptions: {
-      defaultIsModuleExports(id) {
-        try {
-          const module = require(id);
-          if (module?.default) {
-            return false;
-          }
-          return 'auto';
-        } catch (error) {
-          return 'auto';
-        }
+  plugins: [react({babel: {
+        plugins: [
+          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
+        ],
       },
-      transformMixedEsModules: true,
-    },
-  },
+  })],
 })
