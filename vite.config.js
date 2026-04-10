2788ec1696280818235e9ac0f2ddf81fe10a13b8
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-10 17:42:30 -0400

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default {
+
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {}
-  },
-  base: './',
-}
+})
