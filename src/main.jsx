ca3db0bba40b3a3e330c0b2628665796867fdf04
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-10 13:46:37 -0400

    rename to jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { QuizContextProvider } from '../QuizContext.js'
+import { QuizContextProvider } from '../QuizContext.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient({
