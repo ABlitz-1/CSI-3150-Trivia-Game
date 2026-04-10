409a85f68e6798aa9c551e5946ef2d4fc41a9ab6
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-10 18:36:23 -0400

    fix imports

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from 'react'
-import QuizContext from './src/QuizContext'
+import QuizContext from '../QuizContext'
 
 const shuffle = (array) => {
   return array.sort(() => Math.random() - 0.5)
