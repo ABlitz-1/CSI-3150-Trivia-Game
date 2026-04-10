87137d75aca9a6c1b1666b69026edd5507092987
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-10 18:33:38 -0400

    fix imports

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from 'react'
-import QuizContext from './src/QuizContext'
+import QuizContext from '../QuizContext'
 import { useNavigate } from 'react-router-dom'
 
 const Result = () => {
