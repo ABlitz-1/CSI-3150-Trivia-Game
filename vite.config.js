412d4ec520e4f152245c9fa639a3b4d8393c0012
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-10 20:39:05 -0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react({ jsxRuntime: 'classic' })],
-  optimizeDeps: {
-    esbuildOptions: {
-      jsx: "automatic",
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  return {
+    resolve: {
+      alias: {
+       // I needed this to make dev mode work.
+        'react/jsx-runtime': 'react/jsx-runtime.js',
+      },
+    },
+    plugins: [react({
+      babel: {
+        plugins: [
+          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
+        ]
+      },
+    })],
+  };
+});
