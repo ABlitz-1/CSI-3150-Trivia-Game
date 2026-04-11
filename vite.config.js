e1323e1edf84186c26c6ce53670e03c7b640c564
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-10 21:53:09 -0400

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()]
+    plugins: [react()],
+    mode: "development",
+  build: {
+    minify: false,
+  }
   }
 });
