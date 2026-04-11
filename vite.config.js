d7c3f12078473495748bd985b1600581890d4bc6
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-10 20:48:17 -0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   return {
-    resolve: {
-      alias: {
-       // I needed this to make dev mode work.
-        'react/jsx-runtime': 'react/jsx-runtime.js',
-      },
-    },
     plugins: [react({
       babel: {
         plugins: [
