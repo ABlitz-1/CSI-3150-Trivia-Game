46cd25741878c1dac24f6f70aba161a45deb0bd4
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-12 17:56:16 -0400

    use ref for onTimeout to prevent resetting on every re-render

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,23 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 const Timer = ({ onTimeout, timeLimit, questionId }) => {
   const [timeLeft, setTimeLeft] = useState(timeLimit)
+  const onTimeoutRef = useRef(onTimeout)
+
+  useEffect(() => {
+    onTimeoutRef.current = onTimeout
+  }, [onTimeout])
 
   useEffect(() => {
 
     setTimeLeft(timeLimit)
     const timerInt = setInterval(() => {
       setTimeLeft((prev) => {
-        if (prev === 0) {
+        if (prev <= 1) {
           clearInterval(timerInt)
-          onTimeout()
+          onTimeoutRef.current()
           return 0;
         }
         return prev - 1;
       })
     }, 1000)
     return () => clearInterval(timerInt)
-  }, [questionId, timeLimit, onTimeout])
+  }, [questionId, timeLimit])
 
   return (<div>{timeLeft}s</div>)
 }
