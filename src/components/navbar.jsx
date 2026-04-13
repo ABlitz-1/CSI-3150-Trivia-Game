19c83c0313d5fed50dcc90eb3fb20a863e72eae7
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-13 13:56:47 -0400

    fixed typo in navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="navbar bg-none">
       <div className="container">
-        <a clasName="navbar-brand font-impact text-light fw-bolder fs-3" style={{fontSize: "14px"}} href="/">
+        <a className="navbar-brand font-impact text-light fw-bolder fs-3" style={{fontSize: "14px"}} href="/">
           Guess It Trivia
         </a>
       </div>
