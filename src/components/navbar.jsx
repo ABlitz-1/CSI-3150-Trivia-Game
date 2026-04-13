a865af1d60adaedfa7c02fbce13eb136b89f70f4
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-04-13 15:18:52 -0400

    update navbar background

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="navbar bg-none border-bottom border-dark">
+    <nav className="navbar border-bottom border-dark" style={{ backgroundColor: "rgb(120, 120, 120)" }}>
       <div className="container">
         <a className="navbar-brand font-impact text-light fw-bolder fs-3" style={{fontSize: "14px"}} href="/">
           Guess It Trivia
